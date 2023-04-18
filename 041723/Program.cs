b67808e290101257d5504eb287b8cb424f1b7477
Meet Patel <meetpatel3299@icloud.com>
2023-04-18 10:44:02 -0500

    Code Challenge 041723

---
+++ 041723/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+public class CodeChallenge041723{
+
+    private static List<int> FindIndexOfCapitalLetters (string s) {
+        List<int> list = new List<int>();
+        foreach (char ch in s) {
+            if(char.IsUpper(ch)) {
+                list.Add(s.IndexOf(ch));
+            }
+        }
+        return list;
+    }
+    public static void Main(String[] args){
+        Console.WriteLine("Input String : ");
+        string s = Console.ReadLine();
+        Console.WriteLine("Output is : ");
+        Console.WriteLine("[" + string.Join(", ", FindIndexOfCapitalLetters(s)) + "]");
+    }
+}
